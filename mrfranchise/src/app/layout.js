8d103060cbd070989ba6b65971582b1d54c74d04
rajanR14042000 <rajanravi144@gmail.com>
2026-04-11 18:31:26 +0530

    homepage worked files

--- mrfranchise/src/app/layout.js
+++ mrfranchise/src/app/layout.js
@@ -4,25 +4,30 @@ import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import Script from "next/script";
 import RefreshSync from "@/Components/Refresh/RefreshSync";
 import ThemeRegistry from "@/Utils/ThemeRegistry";
+
 export const metadata = {
   title: "Franchise Opportunities in India | Buy Franchise Business | Mr Franchise India – India's #1 Franchise & Business Opportunities Marketplace | Top Franchise Brands in 2024",
   description: "Discover top franchise opportunities in India across food, retail, education, services & more. Buy franchise business with low investment and connect with brands instantly via WhatsApp.",
-  viewport: "width=device-width, initial-scale=1",
   icons: {
     icon: "/mrfranchise_logo.avif",
   },
+  
 };
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>Franchise Opportunities in India | Buy Franchise Business | Mr Franchise India – India's #1 Franchise & Business Opportunities Marketplace | Top Franchise Brands in 2024</title>
-        <meta name="description" content="Discover top franchise opportunities in India across food, retail, education, services & more. Buy franchise business with low investment and connect with brands instantly via WhatsApp." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/mrfranchise_logo.avif" type="image/jpeg" />
-        <link rel="preconnect" href="https://pub-172d0514c5be40ae8cb05f4bd8a1c0ee.r2.dev/" />
-        <link rel="preload" as="font" href="/fonts/inter.woff2" crossorigin />
-      </head>
+   <head>
+  <link rel="preconnect" href="https://pub-172d0514c5be40ae8cb05f4bd8a1c0ee.r2.dev" />
+  <link rel="dns-prefetch" href="https://pub-172d0514c5be40ae8cb05f4bd8a1c0ee.r2.dev" />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/inter.woff2"
+    type="font/woff2"
+    crossOrigin="anonymous"
+  />
+</head>
       <body>
         {/* Scripts */}
         <Script
@@ -31,11 +36,7 @@ export default function RootLayout({ children }) {
           src={`https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
         />
 
-        <Script
-          id="ga-script"
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        />
+        
 
         <Script id="ga-init" strategy="afterInteractive">
           {`
